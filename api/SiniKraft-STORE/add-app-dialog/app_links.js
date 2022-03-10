279a37b06d58a987f0e45c34b33cff833c6c00d8
SiniKraft <natclaudix@gmail.com>
2022-03-10 20:45:26 +0100

    Update SiniKraft STORE api

---
+++ api/SiniKraft-STORE/add-app-dialog/app_links.js
@@ -0,0 +1,13 @@
+function open_link(repo) {
+  api_url = 'https://api.github.com/repos/SiniKraft/' + repo + '/releases/latest';
+  var final_link = 'javascript:void(0)';
+  $.getJSON(api_url, function(json) {
+  	var assets = json.assets;
+    for (let key in assets) {
+    	if (assets[key].browser_download_url.includes('.exe') || assets[key].browser_download_url.includes('.msi')) {
+    		final_link = assets[key].browser_download_url;
+    	}
+    }
+    window.location.replace(final_link);
+  });
+}
